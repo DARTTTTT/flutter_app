c3665bebbbe36b7c9fce895bb427e9350ac82062
DARTTTTT <wengruigan@outlook.com>
2019-09-03 13:54:10 +0800

    banner

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,86 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 void main() => runApp(MyApp());
+
+class SwiperView extends StatefulWidget {
+  @override
+  _SwiperViewState createState() => _SwiperViewState();
+}
+
+class _SwiperViewState extends State<SwiperView> {
+  List<Widget> imgeList = List();
+
+  @override
+  void initState() {
+    imgeList
+      ..add(Image.network(
+        "https://julian.oss-cn-beijing.aliyuncs.com/zy/ZY新版 升级.png",
+        fit: BoxFit.cover,
+      ))
+      ..add(Image.network(
+        "https://julian.oss-cn-beijing.aliyuncs.com/zy/ZY新版 128.png",
+        fit: BoxFit.cover,
+      ));
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("轮播图"),
+      ),
+      body: ListView(
+        children: <Widget>[firstSwiperView(2)],
+        
+      ),
+    );
+  }
+
+  Widget firstSwiperView(int size) {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(0, 0, 0, 5),
+      width: MediaQuery.of(context).size.width,
+      height: 200,
+      child: Swiper(
+        itemCount: size,
+        itemBuilder: _swiperBuilder,
+        pagination: SwiperPagination(
+            alignment: Alignment.bottomRight,
+            margin: const EdgeInsets.fromLTRB(0, 0, 20, 10),
+
+           builder: FractionPaginationBuilder(
+             color: Colors.white,
+             activeColor: Colors.redAccent,
+             activeFontSize: 20,
+               fontSize: 15
+           )
+
+
+           /* builder: DotSwiperPaginationBuilder(
+                color: Colors.grey,
+                activeColor: Colors.white,
+                activeSize: 6,
+                space: 5,
+                size: 6)*/
+        ),
+        controller: SwiperController(),
+        scrollDirection: Axis.horizontal,
+        autoplay: true,
+        onTap: (index) => debugPrint('点击了第$index'),
+      ),
+    );
+  }
+
+  Widget _swiperBuilder(BuildContext context, int dex) {
+    return (imgeList[dex]);
+  }
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -182,13 +261,12 @@ class TapboxA extends StatefulWidget {
   _TapboxAState createState() => new _TapboxAState();
 }
 
-class _TapboxAState extends State<TapboxA>{
+class _TapboxAState extends State<TapboxA> {
+  bool _active = false;
 
-  bool _active=false;
-
-  void _handleTap(){
+  void _handleTap() {
     setState(() {
-      _active=!_active;
+      _active = !_active;
     });
   }
 
@@ -198,22 +276,19 @@ class _TapboxAState extends State<TapboxA>{
     return new GestureDetector(
       onTap: _handleTap,
       child: new Container(
-        child:  new Center(
-          child:  new Text(
-            _active?'Active':'Inactive',
-            style: new TextStyle(fontSize:  32.0,color: Colors.white),
+        child: new Center(
+          child: new Text(
+            _active ? 'Active' : 'Inactive',
+            style: new TextStyle(fontSize: 32.0, color: Colors.white),
           ),
         ),
         width: 60.0,
         height: 30.0,
         decoration: new BoxDecoration(
-          color: _active?Colors.lightBlue[700]:Colors.grey[600]
-        ),
+            color: _active ? Colors.lightBlue[700] : Colors.grey[600]),
       ),
-
     );
   }
-
 }
 
 class CupertinoTestRoute extends StatelessWidget {
@@ -258,7 +333,7 @@ class Echo extends StatelessWidget {
             builder: (BuildContext context)=>new AlertDialog(title: new Text("请输入"),));*/
 
                   Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return TapboxA();
+                    return SwiperView();
                   }));
 
                   /* ScaffoldState _state=context.ancestorStateOfType(
@@ -270,7 +345,79 @@ class Echo extends StatelessWidget {
 
                   );*/
                 },
-                child: new Text("点击一下"))
+                child: new Text(
+                  "点击一下",
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                )),
+            new Text(
+              "textAlign：文本的对齐方式",
+              textAlign: TextAlign.center,
+              maxLines: 2,
+              textScaleFactor: 1.5,
+              overflow: TextOverflow.fade,
+            ),
+            new Text(
+              "HELLO KITTY",
+              style: TextStyle(
+                color: Colors.blue,
+                background: new Paint()..color = Colors.amber,
+                fontSize: 18,
+                height: 2.2,
+                fontFamily: "Courier",
+                decoration: TextDecoration.underline,
+                decorationStyle: TextDecorationStyle.dashed,
+              ),
+            ),
+            new Text.rich(TextSpan(children: [
+              TextSpan(
+                  text: "Home:",
+                  style: TextStyle(
+                    fontSize: 16,
+                    background: new Paint()..color = Colors.grey,
+                    color: Colors.white,
+                  )),
+              TextSpan(
+                  text: "https://flutterchina.club",
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 18,
+                  ))
+            ])),
+            RaisedButton.icon(
+              icon: Icon(Icons.send),
+              label: Text("发送"),
+              onPressed: () {},
+            ),
+            OutlineButton.icon(
+              icon: Icon(Icons.add),
+              label: Text("添加"),
+              onPressed: () {},
+            ),
+            FlatButton.icon(
+                onPressed: () {}, icon: Icon(Icons.info), label: Text("详情")),
+            IconButton(
+              icon: Icon(Icons.thumb_up),
+              onPressed: () {},
+            ),
+            DefaultTextStyle(
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.red,
+              ),
+              textAlign: TextAlign.start,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  new Text("HELLO WORLD"),
+                  new Text("O(∩_∩)O哈哈~"),
+                  new Text(
+                    "O(∩_∩)O哈哈~",
+                    style: TextStyle(color: Colors.grey, inherit: false),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
