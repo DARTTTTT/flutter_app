0899beba6a5253e8a7f2fb7c98762c1f58a550f9
aaron <aaronstepbystep@gmail.com>
2019-12-16 17:59:58 +0800

    移植Android x

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:news/config/manger.dart';
-import 'package:news/entity/Content.dart';
 import 'package:news/model/theme_model.dart';
 import 'package:oktoast/oktoast.dart';
 import 'package:provider/provider.dart';
@@ -13,21 +12,15 @@ import 'manger/provider_manager.dart';
 export 'package:provider/provider.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   Provider.debugCheckInvalidValueType = null;
   await AppManger.init();
-
-
-
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
-
-
   @override
   Widget build(BuildContext context) {
-
     // TODO: implement build
     return OKToast(
         child: MultiProvider(
